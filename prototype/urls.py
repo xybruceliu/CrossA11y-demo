2add228ba0400ae9da7bac3f7f9307356254e1ba
Qing Shi <aliceshi0804@gmail.com>
2021-10-14 21:44:48 -0700

    pass dictionary to template

--- prototype/urls.py
+++ prototype/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:video_id>/', views.video, name='video'),
+    path('<str:video_id>/detail', views.video_detail, name='video_detail'),
     path('<str:video_id>/add/', views.add, name='add'),
 ]
